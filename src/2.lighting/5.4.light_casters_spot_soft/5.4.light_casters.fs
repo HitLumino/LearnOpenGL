f289cb6f78c9f82bfa54f827039752e51175cee3
HitLumino <1095916953@qq.com>
2021-04-02 00:57:48 +0800

    适配 ies shader

--- src/2.lighting/5.4.light_casters_spot_soft/5.4.light_casters.fs
+++ src/2.lighting/5.4.light_casters_spot_soft/5.4.light_casters.fs
@@ -31,16 +31,63 @@ uniform Material material;
 uniform Light light;
 uniform sampler2D ies;
 
+#define PI 3.1415926535897932384626433832795
+
+vec3 getLumin(vec3 WorldPosition, vec3 LightPosition, vec3 LightDirection, vec3 LightUp) {
+    vec3 LightTangent = normalize(cross(LightUp, LightDirection));
+    vec3 LightBiTangent = normalize(cross(LightDirection, LightTangent));
+    mat4 LightTransform = mat4(vec4(LightDirection.xyz, 0.0), vec4(LightTangent.xyz, 0.0), vec4(LightBiTangent.xyz, 0.0), vec4(0.0, 0.0, 0.0, 1.0));
+    mat4 InvLightTransform = transpose(LightTransform);
+    vec3 ToLight = normalize(LightPosition - WorldPosition);
+    vec3 LocalToLight = (InvLightTransform * vec4(ToLight.xyz, 0.0)).xyz;
+    
+    float DotProd = dot(ToLight, LightDirection);
+    float AngleV = asin(DotProd);
+    
+    // 0...1
+    float NormAngleV = AngleV / PI + 0.5f;
+    
+    float AngleH = atan(-LocalToLight.y, -LocalToLight.z);
+    float NormAngleH = AngleH / (PI * 2.f) + 0.5f;
+    
+    return texture(ies, vec2(NormAngleV, NormAngleH)).rgb;
+}
+
 void main()
 {
     // ambient
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
-    
+
+// //////////////////////
+//
+//     vec3 LightTangent = normalize(cross(vec3(0.f, 0.f, 1.f), light.direction));
+//     vec3 LightBiTangent = normalize(cross(light.direction, LightTangent));
+//     mat4 LightTransform = mat4(vec4(light.direction.xyz, 0.0), vec4(LightTangent.xyz, 0.0), vec4(LightBiTangent.xyz, 0.0), vec4(0.0, 0.0, 0.0, 1.0));
+//     mat4 InvLightTransform = transpose(LightTransform);
+//     vec3 ToLight = normalize(light.position - FragPos);
+//     vec3 LocalToLight = (InvLightTransform * vec4(ToLight.xyz, 0.0)).xyz;
+//
+//     float DotProd = dot(ToLight, light.direction);
+//     float AngleV = acos(DotProd);
+//
+//     // 0...1
+//         float NormAngleV = AngleV / PI + 0.5f;
+//
+//         float AngleH = atan(-LocalToLight.y, -LocalToLight.z);
+//         float NormAngleH = AngleH / (PI * 2.f) + 0.5f;
+//
+//     vec3 multi = texture(ies, vec2(NormAngleV, NormAngleH)).rgb;
+//
+// //////////////////////
+
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
+    vec3 diffuseMutify = getLumin(FragPos, light.position, light.direction, vec3(0.f, 0.4f, 1.f));
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+    vec3 diffuse = 100 * diffuseMutify * diff * texture(material.diffuse, TexCoords).rgb;
+    // 指定值
+//  vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
